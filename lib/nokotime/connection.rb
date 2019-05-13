0feccb181df236b97c002f8759547d8f483a8e27
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-05-13 14:35:22 +0200

    Add connection class and spec

---
+++ lib/nokotime/connection.rb
@@ -0,0 +1,83 @@
+module Nokotime
+  class Connection
+    include Nokotime::Authentication
+
+    module Errors
+      class ConnectionFailed < StandardError; end
+      class ResourceNotFound < StandardError; end
+      class ClientError < StandardError; end
+    end
+
+    # rubocop:disable Metrics/MethodLength
+    def get(path, params: {}, request_options: {})
+      response = connection.get do |request|
+        authorize_request(request)
+        set_request_options(request, request_options)
+        request.url path, params
+      end
+
+      response
+    rescue Faraday::ConnectionFailed => e
+      raise Errors::ConnectionFailed.new(e), e.message
+    rescue Faraday::ResourceNotFound => e
+      raise Errors::ResourceNotFound.new(e), e.message
+    rescue Faraday::ClientError => e
+      raise Errors::ClientError.new(e), e.message
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    def get_in_parallel(
+      path,
+      from_page_number,
+      to_page_number,
+      params: {},
+      request_options: {}
+    )
+      responses = []
+
+      connection.in_parallel(manager) do
+        (from_page_number..to_page_number).each do |page|
+          merged_params = {page: page}.merge(params)
+
+          responses << get(
+            path, params: merged_params, request_options: request_options
+          )
+        end
+      end
+
+      responses
+    end
+
+    private
+
+    def connection
+      @connection ||= Faraday.new(default_options) do |connection|
+        connection.request  :json
+        connection.response :json, content_type: /\bjson$/
+        connection.response :raise_error
+        connection.adapter  :typhoeus
+      end
+    end
+
+    def default_options
+      {
+        url: Nokotime.configuration.url,
+        headers: {
+          user_agent: "MyNokoBot/1.0",
+          accept: "application/json"
+        }
+      }
+    end
+
+    def set_request_options(request, options)
+      request.options.timeout = options[:timeout]
+      request.options.open_timeout = options[:open_timeout]
+    end
+
+    def manager
+      @manager ||= Typhoeus::Hydra.new(
+        max_concurrency: Nokotime.configuration.max_concurrency
+      )
+    end
+  end
+end
---
+++ spec/nokotime/connection_spec.rb
@@ -0,0 +1,106 @@
+require "spec_helper"
+
+RSpec.describe Nokotime::Connection, :vcr do
+  subject(:request) { described_class.new }
+
+  let(:response) { request.get(end_point, params: params) }
+  let(:end_point) { "/v2/users" }
+
+  describe "with default configuration" do
+    let(:params) { {} }
+    let(:ua) { response.env.request_headers["User-Agent"] }
+    let(:header_token) do
+      response.env.request_headers["X-NokoToken"]
+    end
+
+    it "responses be in json format" do
+      expect(response.env.response_headers["Content-type"]).to(
+        eq("application/json; charset=utf-8")
+      )
+    end
+
+    it "set user agent" do
+      expect(ua).to eq("MyNokoBot/1.0")
+    end
+
+    it "set X-NokoToken" do
+      expect(header_token).not_to be_nil
+    end
+
+    context "with page param" do
+      let(:params) { {per_page: 1, page: 2} }
+
+      it "responses OK" do
+        expect(response.success?).to be(true)
+      end
+    end
+
+    context "with per_page param" do
+      let(:per_page) { 8 }
+      let(:params) { {per_page: per_page} }
+
+      it "responses have per_page elements" do
+        expect(response.body.count).to eq(per_page)
+      end
+    end
+  end
+
+  describe "with excpections" do
+    let(:end_point) { "/invalid_url" }
+    let(:params) { {} }
+
+    it "raises a resource not found error for invalid resource" do
+      expect do
+        response
+      end.to raise_error(Nokotime::Connection::Errors::ResourceNotFound)
+    end
+
+    it "returns status code" do
+      expect do
+        response
+      end.to raise_error do |error|
+        expect(error.cause.response[:status]).to eq(404)
+      end
+    end
+
+    context "with connection failed" do
+      before do
+        allow(Faraday).to receive(:new).once.and_raise(
+          Faraday::ConnectionFailed, "Connection failed"
+        )
+      end
+
+      it "raises a connection failed exception" do
+        expect do
+          response
+        end.to raise_error(Nokotime::Connection::Errors::ConnectionFailed)
+      end
+    end
+
+    context "with client error" do
+      before do
+        allow(Faraday).to receive(:new).once.and_raise(
+          Faraday::ClientError, "Client Error"
+        )
+      end
+
+      it "raises a client error exception" do
+        expect do
+          response
+        end.to raise_error(Nokotime::Connection::Errors::ClientError)
+      end
+    end
+
+    context "with too many request" do
+      # per_page = 1 raise Faraday::ClientError: the server responded
+      # with status 429 (Too Many Requests)
+      #
+      # HTTP/1.1 429 Too Many Requests
+      # Content-Type: text/html
+      # Retry-After: 3600
+      # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/429
+
+      xit "to implement"
+    end
+  end
+end
