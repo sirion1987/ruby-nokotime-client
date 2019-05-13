56b03e2c6094a7c2fa8f9461a63cb3a10e63d902
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-05-13 14:37:59 +0200

    Include nokotime files

--- lib/nokotime.rb
+++ lib/nokotime.rb
@@ -1,22 +1,30 @@
 require "dry-validation"
+require "faraday"
+require "faraday_middleware"
 
+require "nokotime/authentication"
 require "nokotime/configuration"
+require "nokotime/connection"
+require "nokotime/paginator"
+require "nokotime/request/single_page"
+require "nokotime/request/multiple_pages"
 require "nokotime/validator/restricted_hash"
 require "nokotime/validator/user"
 require "nokotime/client/users"
 
 module Nokotime
-  module_function
-
-  def configure
-    yield configuration if block_given?
+  def configure(&block)
+    yield configuration
   end
+  module_function :configure
 
   def configuration
     @configuration || reset
   end
+  module_function :configuration
 
   def reset
     @configuration = Configuration.new
   end
+  module_function :reset
 end
