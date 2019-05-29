9ad96288cebade8710c10cddc039fc7063b63e1f
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-05-29 08:00:47 +0200

    Refactor project group spec

--- spec/nokotime/client/project_groups_spec.rb
+++ spec/nokotime/client/project_groups_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe Nokotime::Client::ProjectGroups, :vcr do
   subject(:client) { described_class.new(params) }
 
-  let(:expected_project_group_keys) do
+  let(:expected_keys) do
     a_hash_including(
       "id",
       "name",
@@ -20,27 +20,10 @@ RSpec.describe Nokotime::Client::ProjectGroups, :vcr do
   end
 
   describe "#all" do
-    let(:result) { client.all }
+    let(:responses) { client.all }
     let(:params) { {} }
 
-    it "returns a Request::MultiplePages" do
-      expect(result).to be_a(Nokotime::Request::MultiplePages)
-    end
-
-    it "returns a valid project groups" do
-      expect(result.last_responses.first.body.first).to(
-        match(expected_project_group_keys)
-      )
-    end
-
-    describe "with wrong params" do
-      let(:params) { {unknow_param: "invalid_param"} }
-
-      it "raises a invalid params error" do
-        expect { result }.to(
-          raise_error(Nokotime::Errors::InvalidParams)
-        )
-      end
-    end
+    it_behaves_like "a multiple pages"
+    it_behaves_like "an endpoint with valid parameters"
   end
 end
